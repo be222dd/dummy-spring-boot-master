69b2650ba1660f9d6222bf3ad7a963c2c5c4c79a
be222dd <beysimeryilmaz@gmail.com>
2020-01-17 18:18:07 +0300

    plugin problem 3

--- src/test/java/com/gazgeek/helloworld/HealthTest.java
+++ src/test/java/com/gazgeek/helloworld/HealthTest.java
@@ -6,33 +6,39 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebIntegrationTest(randomPort = true)
-public class HomeControllerTest {
+public class HealthTest {
 
     @Value("${local.server.port}")
     private Integer port;
 
     private RestTemplate restTemplate = new TestRestTemplate();
 
+
     @Test
-    public void helloWorld() {
-        getRequest("/")
+    public void checkHealth() {
+        getRequest("/health")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello from bestcloudforme!");
+            .assertResponseBody("{\"status\":\"UP\"}");
     }
 
-    private HelloWorldResponse getRequest(String uri) {
-        return new HelloWorldResponse(restTemplate.getForEntity(getUri(uri), String.class));
+
+    private HealthResponse getRequest(String uri) {
+        return new HealthResponse(restTemplate.getForEntity(getUri(uri), String.class));
     }
 
 
@@ -47,4 +53,23 @@ public class HomeControllerTest {
             .toUri();
     }
 
+
+    private static class HealthResponse {
+
+        private ResponseEntity<String> responseEntity;
+
+        public HealthResponse(ResponseEntity<String> responseEntity) {
+            this.responseEntity = responseEntity;
+        }
+
+        public HealthResponse assertStatusCode(HttpStatus expected) {
+            assertThat(responseEntity.getStatusCode(), is(expected));
+            return this;
+        }
+
+        public HealthResponse assertResponseBody(String expected) {
+            assertThat(responseEntity.getBody(), is(expected));
+            return this;
+        }
+    }
 }
